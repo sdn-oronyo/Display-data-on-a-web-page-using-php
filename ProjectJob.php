47e3872a691ad66fab80dbf0000c95f5c6210b16
MD. Mehedi Hasan <emonmehedi61@gmail.com>
2018-05-16 15:38:44 +0600

    added dbname and indentation

--- ProjectJob.php
+++ ProjectJob.php
@@ -10,7 +10,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check Connection
 if ($conn->connect_error) {
 	die ("Connection faild: " . $conn->connect_error);
-	}
+}
 
 $sql = "SELECT id, firstname, lastname FROM MyGuests";
 $result = $conn->query($sql);
@@ -20,7 +20,6 @@ if ($result->num_rows > 0) {
 	while($row = $result->fetch_assoc()) {
 		echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
 	}
-
 }
 
 else {
