56862bce043c952313e4199bc720849e6eaf55ba
Sreekantha Dev Nath <37050179+sdn-oronyo@users.noreply.github.com>
2018-05-16 17:36:19 +0600

    Update db.sql

--- src/db.sql
+++ src/db.sql
@@ -3,13 +3,12 @@ SET time_zone = "+00:00";
 
 -- Database Name: `db`
 --
--- Table structure for table `users`
+-- Table structure for table `MyGuests`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE IF NOT EXISTS `MyGuests` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
